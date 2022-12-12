ddc62afd28cf6ff316c4cbd92f3f15558a77f3ab
Juan <juandelaportillacardenas@gmail.com>
2022-12-12 16:04:47 -0500

    deleted even more unnecesary files

--- src/main/java/com/squawk/webapp/repositories/countByMonthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.squawk.webapp.repositories;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface countByMonthRepository<T>{
-    List<T> findCantCuacks() throws SQLException;
-    List<T> findCantUsers() throws SQLException;
-}
